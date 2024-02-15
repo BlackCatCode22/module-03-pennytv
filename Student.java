9b2d826f079624cbd5d32c7093338993643f4be9
pennytv <157195845+pennytv@users.noreply.github.com>
2024-02-14 20:56:31 -0800

    Add files via upload

--- Student.java
+++ Student.java
@@ -1,9 +1,8 @@
-package mystudent;
-
-public class Student {
-    String firstName;
-    String lastname;
+public class Student
+{String firstName;
+    String lastName;
     double gpa;
     String major;
-    String city;
+    int age;
+    boolean onProbation;
 }
